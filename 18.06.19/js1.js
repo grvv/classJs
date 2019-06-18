2eb9ed3644589f38c983c4d10df0dd9d0396e97e
Gaurav Sharma <gaurav.sharma@zenways.io>
2019-06-18 12:02:49 +0530

    First file Added

---
+++ 18.06.19/js1.js
@@ -0,0 +1,73 @@
+// console.log('hello World!')
+// var, let , const
+// const str = 'hello';
+// str = 'abcd';
+
+// var abcd = 'hello world';
+// let abcde = ' some value';
+
+// var boolValue = true  
+// var boolValue = false;  
+
+// let num = 1.23;
+// let num1 = 132;
+
+let car = {
+    make: 'honda',
+    yearOfMan: 2018
+};
+
+// console.log(car.yearOfMan);
+
+// let arr = ['a', 1, car]
+
+// console.log(arr[0])
+// console.log(arr.length)
+// console.log(arr[arr.length - 1])
+
+// undefined;
+
+// console.log(abc);
+// var abc = 'hello';
+
+// var abcd = null;
+
+// var obj = {
+//     firstProp: 'Hello'
+// }
+// // some calculation
+
+// obj = null;
+
+// let abc = 0;
+
+// console.log(abc)
+
+// falsy values 
+// 0, undefined, null, '', false
+// var condition = 'abcd';
+
+// if(condition){
+//     console.log('Condition is truthy')
+// }else{
+//     console.log('Condition is falsy')
+// }
+
+var num1 = 123;
+// var num2 = 10;
+
+// console.log(num1 % num2);
+
+var str1 = 'hello';
+// var str2 = 'World';
+
+// console.log(str1 + ' ' +str2);
+
+// var arr = [];
+// let obj = {}
+
+// console.log(typeof arr)
+// console.log(typeof obj)
+
+
+
