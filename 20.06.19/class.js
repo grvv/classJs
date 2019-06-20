a87dd51871aa128b3166a1e1fdf7e24a95b10c7a
Ankur Atri <ankuratri@hotmail.com>
2019-06-20 11:11:08 +0530

    Strings

---
+++ 20.06.19/class.js
@@ -0,0 +1,15 @@
+// Loops
+
+// While Loop
+let condition = 1;
+
+// while (condition < 5) {
+//   console.log("Hello World!");
+//   condition++;
+// }
+
+// Do while
+// do {
+//   console.log("Hello World!");
+//   condition++;
+// } while (condition < 5);
---
+++ 20.06.19/strings.js
@@ -0,0 +1,17 @@
+// let myStr = "Hello";
+// let myStr = "Hello";
+// // let newStr = "What's up";
+// // let newStr = "What's up";
+
+// const someExpression = "Class";
+
+// let num1 = 5;
+// let num2 = 10;
+
+// // First Method
+// let combinedStr = myStr + " " + someExpression;
+
+// let tempLitrals = `Hello ${num1 * num2}`;
+
+// console.log("First --", combinedStr);
+// console.log("Last --", tempLitrals);
