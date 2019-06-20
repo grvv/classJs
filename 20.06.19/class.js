b47382203036cbed7cef96f8835ba253aeba5f65
Ankur Atri <ankuratri@hotmail.com>
2019-06-20 13:11:52 +0530

    Functions

--- 20.06.19/class.js
+++ 20.06.19/class.js
@@ -1,7 +1,7 @@
 // Loops
 
 // While Loop
-let condition = 1;
+// let condition = 1;
 
 // while (condition < 5) {
 //   console.log("Hello World!");
@@ -13,3 +13,39 @@ let condition = 1;
 //   console.log("Hello World!");
 //   condition++;
 // } while (condition < 5);
+
+// let key = "Hello";
+
+// switch (key) {
+//   case "Hello":
+//     console.log("Hello Case");
+//     break;
+//   case "World":
+//     console.log("World Case");
+//     break;
+//   default:
+//     console.log("Default");
+//     break;
+// }
+
+// Functions
+// console.log(square(8));
+
+// function square(num) {
+//   console.log(num * num);
+// }
+
+// console.log(myFun(5));
+
+// var myFun = function(num) {
+//   return num * num;
+// };
+
+// let myFn = (num1, num2) => num1 * num2;
+
+// console.log(myFn(5, 2));
+
+// (function myFn(num) {
+//   console.log(num);
+//   return num * num;
+// })(2);
