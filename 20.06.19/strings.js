1e394e33d00a9aebe60936337242e8602f11bff1
Ankur Atri <ankuratri@hotmail.com>
2019-06-20 11:32:24 +0530

    String method

--- 20.06.19/strings.js
+++ 20.06.19/strings.js
@@ -8,10 +8,24 @@
 // let num1 = 5;
 // let num2 = 10;
 
-// // First Method
+// First Method
 // let combinedStr = myStr + " " + someExpression;
 
 // let tempLitrals = `Hello ${num1 * num2}`;
 
 // console.log("First --", combinedStr);
 // console.log("Last --", tempLitrals);
+
+// let newString = "Hello world!";
+// let extraString = "hi";
+// console.log(newString.length);
+// console.log(newString.indexOf("e"));
+// console.log(newString.charAt(2));
+// console.log(newString.concat(extraString));
+
+let sentence = "The quick brown fox jumps over the lazy dog.";
+// console.log(sentence.includes("fox"));
+// console.log(sentence.includes("hello"));
+// console.log(sentence.lastIndexOf("brown"));
+// console.log(sentence.indexOf("brown") + 5);
+console.log(sentence.slice(0, 15));
