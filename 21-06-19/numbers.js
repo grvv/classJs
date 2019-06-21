489675fbe2072784d32d8d60164845cf0853ed3d
Ankur Atri <ankuratri@hotmail.com>
2019-06-21 11:17:50 +0530

    Number type

---
+++ 21-06-19/numbers.js
@@ -0,0 +1,33 @@
+// let num = 10;
+// let numDec = 1.23;
+
+// console.log(typeof num, typeof numDec);
+
+// let num = "123";
+// let parsedInt = parseInt(num);
+// // console.log(typeof num);
+// console.log(parsedInt);
+
+// console.log(num)
+
+// let floatNum = "10.23";
+// let parsedFloat = parseFloat(floatNum);
+
+// console.log(typeof parsedFloat);
+
+// let num = 123;
+// let str = "abc";
+// let multiplicationRes = num * str;
+
+// console.log(typeof multiplicationRes);
+// console.log(isNaN(multiplicationRes));
+// console.log(isNaN(num));
+
+// let testNum = 123456.123456;
+
+// console.log(Math.ceil(testNum));
+// console.log(Math.floor(testNum));
+
+let randomNum = Math.random();
+
+console.log(randomNum);
