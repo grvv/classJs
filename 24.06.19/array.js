ad6fd62c5f5333f86d2092ba3427833351e4e070
Gaurav <gaurav.gauravsharmaa@gmail.com>
2019-06-24 12:45:26 +0530

    map filter reduce

--- 24.06.19/array.js
+++ 24.06.19/array.js
@@ -18,10 +18,21 @@
 // arr.pop();
 // console.log(arr);
 
-const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
+// const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
 
 // console.log(animals.slice(1));
 // console.log(animals);
 
-console.log(animals.splice(2, 0, 'Goat'));
-console.log(animals);
+// console.log(animals.splice(2, 0, 'Goat'));
+// console.log(animals);
+
+const arr = [1, 2, 3, 4, 5];
+
+// const result = arr.map(value => value * 2);
+// const result = arr.filter(value => value > 2);
+// const result = arr.reduce((accValue, currValue, index, array) => {
+//     return accValue + currValue
+// }, 0);
+const result = arr.reduce((accValue, currValue) => accValue + currValue);
+
+console.log(result);
