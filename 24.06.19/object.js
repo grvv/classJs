aeb04cd05cb22071fdc00618a6c5812a2eebf60e
Gaurav <gaurav.gauravsharmaa@gmail.com>
2019-06-24 11:45:55 +0530

    Objects

---
+++ 24.06.19/object.js
@@ -0,0 +1,31 @@
+const myprop = "Something";
+const propName = "just like this";
+
+let myObj = {
+  name: "Name",
+  firstName: "Hello",
+  lastName: "World",
+  myProp1: "MyValue1",
+  [myprop + propName]: "myDynamic prop's value",
+
+  get fullName() {
+    return this.firstName + this.lastName;
+  },
+
+  // set updateName(newName) {
+  //   this.name = newName;
+  // }
+
+  set updateNames(value) {
+    fNameLname = value.split(" ");
+
+    this.firstName = fNameLname[0];
+    this.lastName = fNameLname[1];
+  }
+};
+
+myObj.updateNames = "John Doe";
+
+console.log(myObj);
+// console.log(myObj.fullName);
+// console.log(myObj.updateName);
