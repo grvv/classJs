57b33d36c73eb66e7fd14211bc58ae026f99c337
Gaurav <gaurav.gauravsharmaa@gmail.com>
2019-06-24 12:29:49 +0530

    slice and splice

--- 24.06.19/array.js
+++ 24.06.19/array.js
@@ -1,4 +1,4 @@
-const arr = [1, 2, 3, 4, 5];
+// const arr = [1, 2, 3, 4, 5];
 
 // console.log(arr.length);
 // console.log(arr[0]);
@@ -11,3 +11,17 @@ const arr = [1, 2, 3, 4, 5];
 // arr.forEach( callBackFn )
 
 // arr.forEach((element, index) => console.log(element, index));
+
+// arr.push(6);
+// console.log(arr);
+
+// arr.pop();
+// console.log(arr);
+
+const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
+
+// console.log(animals.slice(1));
+// console.log(animals);
+
+console.log(animals.splice(2, 0, 'Goat'));
+console.log(animals);
