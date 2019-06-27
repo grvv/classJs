3c0662a0a4d687815c5777b89a7a4072316ece13
Gaurav <gaurav.gauravsharmaa@gmail.com>
2019-06-27 13:04:21 +0530

    array destructurig

---
+++ 27.6.19/array.js
@@ -0,0 +1,12 @@
+// const arr = [1, 2];
+
+// // const [first, second, , forth] = arr;
+// const [first, second, third = 10] = arr;
+
+// console.log(first, second, third);
+let a = 10;
+let b = 20;
+
+[a, b] = [b, a];
+
+console.log(a, b);
