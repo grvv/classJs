b86163f2d6970feda4b200beec4367327fc5c853
laptop-00 <Nehashetty@nehashettyn@gmail.com>
2019-07-05 13:49:39 +0530

    Async code

---
+++ 05-07-19/async.js
@@ -0,0 +1,11 @@
+// setTimeout(() => {
+//   console.log("Hello World");
+// }, 1000);
+
+console.log("A");
+console.log("B");
+setTimeout(() => {
+  console.log("C");
+}, 0);
+console.log("D");
+console.log("E");
